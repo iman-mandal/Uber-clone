b737799e049bb3600cd251087692e60250ea12ea
Iman Mandal <mandalim19@gmail.com>
2025-12-15 23:33:52 +0530

    Add user login

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -11,12 +11,31 @@ module.exports.registerUser = async (req, res, next) => {
     const hashPassword = await userMOdel.hashPassword(password);
 
     const user = await userService.createUser({
-        firstname:fullname.firstname,
-        lastname:fullname.lastname, 
-        email, 
+        firstname: fullname.firstname,
+        lastname: fullname.lastname,
+        email,
         password: hashPassword
     });
     console.log("Your Registion is Sucessfully Completed")
-    const token= user.genarateAuthToken();
-    res.status(201).json(token,user);
+    const token = user.genarateAuthToken();
+    res.status(201).json(token, user);
+}
+
+module.exports.loginUser = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(404).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    const user = await userMOdel.findOne({ email }).select('+password');
+    if (!user) {
+        return res.status(401).json({ message: "Invalid Password or Email" });
+    }
+    const isMatch = await (user.comparePassword(password));
+    if (!isMatch) {
+        return res.status(401).json({ message: "Invalid Password or Email" });
+    }
+    const token = user.genarateAuthToken();
+    console.log("Login Sucessfully");
+    res.status(200).json({token,user});
 }
